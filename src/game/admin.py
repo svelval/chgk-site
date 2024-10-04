29fbba9ebedc19325e10d4cc4c1e0a52f72e01dd
svelval <svelval@mail.ru>
2024-10-04 13:08:23 +0500

    Group & Game models added

--- src/game/admin.py
+++ src/game/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Game, Group
+
+
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+    model = Group
+    list_display = ('name',)
+
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    model = Game
+    list_display = ('year', 'group', 'group_index', 'get_teams', 'date', )
--- src/game/models.py
+++ src/game/models.py
@@ -1,3 +1,47 @@
+import datetime
+
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
-# Create your models here.
+from django.utils.translation import gettext_lazy as _
+
+from gamers.models import Team
+
+
+class Group(models.Model):
+    class Meta:
+        verbose_name = _('Group')
+        verbose_name_plural = _('Groups')
+
+    name = models.CharField(unique=True, verbose_name=_('name'))
+
+
+class Game(models.Model):
+    class Meta:
+        verbose_name = _('Game')
+        verbose_name_plural = _('Games')
+        unique_together = ('year', 'group', 'group_index', )
+
+    NAME_CHOICES = (
+        ('first_game', _('First game')),
+        ('second_game', _('Second game')),
+        ('third_game', _('Third game')),
+        ('fourth_game', _('Fourth game')),
+        ('fifth_game', _('Fifth game')),
+        ('final_game', _('Final game')),
+        ('year_finale', _('Year finale')),
+    )
+
+    date = models.DateTimeField(primary_key=True, verbose_name=_('date'))
+    year = models.PositiveIntegerField(validators=[MinValueValidator(2014),
+                                                   MaxValueValidator(datetime.date.today().year)],
+                                       verbose_name=_('year'))
+    group = models.ForeignKey(to=Group, on_delete=models.PROTECT, verbose_name=_('group'))
+    group_index = models.PositiveIntegerField(default=1, verbose_name=_('game index'))
+    name = models.CharField(choices=NAME_CHOICES, verbose_name=_('game name'))
+    teams = models.ManyToManyField(to=Team, verbose_name=_('teams'))
+    connoisseur_score = models.PositiveIntegerField(default=0, verbose_name=_('connoisseur score'))
+    tv_viewers_score = models.PositiveIntegerField(default=0, verbose_name=_('tv viewers score'))
+
+    def get_teams(self):
+        return ', '.join(team for team in self.teams.all())
