638b4c802ded79bb4cdde6a7b1e7ac6f5887697d
svelval <svelval@mail.ru>
2024-10-08 10:24:55 +0500

    GameView added

--- src/game/urls.py
+++ src/game/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('<int:season>/<str:group>/<int:group_index>', views.GameView.as_view(), name='game'),
 ]
 
 
--- src/game/views.py
+++ src/game/views.py
@@ -1,5 +1,22 @@
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import Http404
 from django.shortcuts import render
+from django.views.generic import TemplateView
+
+from .models import Game
 
 
-def index(request):
-    return render(request, 'game/index.html')
+class GameView(TemplateView):
+    template_name = 'game/game.html'
+
+    def get(self, request, *args, **kwargs):
+        try:
+            Game.objects.get(**kwargs)
+        except ObjectDoesNotExist:
+            raise Http404
+        return super(GameView, self).get(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super(GameView, self).get_context_data(**kwargs)
+        context['game'] = Game.objects.get(**kwargs)
+        return context
