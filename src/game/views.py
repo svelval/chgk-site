3e7ee7f482d69d7928ed120bc4eda3fea6507cec
svelval <svelval@mail.ru>
2024-10-09 11:28:01 +0500

    GameView became DetailView child

--- src/game/views.py
+++ src/game/views.py
@@ -1,22 +1,21 @@
 from django.core.exceptions import ObjectDoesNotExist
 from django.http import Http404
-from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import DetailView
 
 from .models import Game
 
 
-class GameView(TemplateView):
+class GameView(DetailView):
     template_name = 'game/game.html'
+    model = Game
 
-    def get(self, request, *args, **kwargs):
+    def get_object(self, queryset=None):
         try:
-            Game.objects.get(**kwargs)
+            return self.model.objects.get(**self.kwargs)
         except ObjectDoesNotExist:
             raise Http404
-        return super(GameView, self).get(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
         context = super(GameView, self).get_context_data(**kwargs)
-        context['game'] = Game.objects.get(**kwargs)
+        context['game'] = self.object
         return context
