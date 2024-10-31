ebc3d9f8b111fbfbe6ab52129b8edd8d97389229
svelval <svelval@mail.ru>
2024-10-31 16:24:52 +0500

    get_absolute_url simple tag added to Game

--- src/game/templatetags/get_attribute.py
+++ src/game/templatetags/get_attribute.py
@@ -8,3 +8,8 @@ def get_attribute(value, arg):
     obj = value
     attr = arg
     return getattr(obj, attr, None)
+
+
+@register.simple_tag(name='get_absolute_url')
+def get_absolute_url(value):
+    return getattr(value, 'get_absolute_url', str)()
